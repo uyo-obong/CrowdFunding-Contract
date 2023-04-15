a7408b3f20f9a81e74fb72c04ba5d00674a8db7f
Uyo-obong Akpan <uyoobonga@gmail.com>
2023-04-15 12:03:24 +0100

    implemented smart contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   solidity: {
     version: '0.8.9',
+    defaultNetwork: 'https://rpc.ankr.com/eth_goerli',
+    networks: {
+      hardhat: {},
+      goerli: {
+        url: 'https://rpc.ankr.com/eth_goerli',
+        accounts: [`0x${process.env.GEORLI_PRIVATE_KEY}`],
+      },
+    },
     settings: {
       optimizer: {
         enabled: true,
